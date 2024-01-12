5270da20e3272d3fd763885376759e8039389763
ARUNKUMARP123 <154363796+ARUNKUMARP123@users.noreply.github.com>
2024-01-12 19:10:06 +0530

    Update movie.js

--- class movie_circle/movie.js
+++ class movie_circle/movie.js
@@ -5,6 +5,10 @@ class movie{
         this.studio=studio;
         this.rating=rating;
     }
+    getPG(){
+        return `the movie and rating is: ${new Array(this.movie,this.studio,this.rating)}`
+}
 }
 var Movie1=new movie("asino Royale","Eon Productions","PG­13");
 console.log(Movie1);
+    console.log(Movie1.getPG());
