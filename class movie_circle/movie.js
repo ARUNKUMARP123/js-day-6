ba55add1b66e367fe29968f8432cf8843a74ffaa
ARUNKUMARP123 <154363796+ARUNKUMARP123@users.noreply.github.com>
2024-04-11 10:31:33 +0530

    Update movie.js
    add cod for 3.Write a “person” class to hold all the details., 4.write a class to calculate the uber price.

--- class movie_circle/movie.js
+++ class movie_circle/movie.js
@@ -12,3 +12,42 @@ class movie{
 var Movie1=new movie("asino Royale","Eon Productions","PG­13");
 console.log(Movie1);
     console.log(Movie1.getPG());
+
+
+
+// 3.Write a “person” class to hold all the details.
+
+class Person{
+    constructor(name, age, gender, state, country){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.state = state;
+        this.country = country;
+    }
+}
+
+let person = new Person("Elango", 21, "Male", "Tamil nadu", "India");
+console.log(`3.Write a “person” class to hold all the details.
+${person.name}
+${person.age}
+${person.gender}
+${person.state}
+${person.country}
+`)
+
+// 4.write a class to calculate the uber price.
+
+const uber = {
+    set kiloMeter(km){    
+       this.price = `${km} km = Rs.${km * 50}` ;
+    },
+    get kiloMeter(){
+       return this.price;
+    }
+}
+
+uber.kiloMeter = 2;
+
+console.log(`4.) write a class to calculate the uber price.
+Answer : ${uber.kiloMeter}`)
